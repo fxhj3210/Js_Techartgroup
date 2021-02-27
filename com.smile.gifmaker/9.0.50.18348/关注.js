6574de51472104dc7cf5c37085af35740a965f7b
fxhj3210 <fxhj3210@sina.com>
2021-02-28 04:41:38 +0800

    1

--- com.smile.gifmaker/9.0.50.18348/关注.js
+++ com.smile.gifmaker/9.0.50.18348/关注.js
@@ -1,13 +1,5 @@
-var EnginesList = []
-EnginesList.push(engines.execScript(Date.parse(new Date()).toString(), 'id("left_btn").findOne(10000).click()'));
-
-
-
-/*
 app.startActivity({
     action: "android.intent.action.VIEW",
-    data: "kwai://profile/1340362263",
+    data: "kwai://webview?url=[https://www.baidu.com]",
     packgeName: "com.smile.gifmaker"
 });
-
-*/
