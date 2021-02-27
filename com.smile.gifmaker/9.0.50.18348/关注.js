16ecbd93e051a9911fec911a8df6afd230691e23
fxhj3210 <fxhj3210@sina.com>
2021-02-28 03:14:59 +0800

    one

---
+++ com.smile.gifmaker/9.0.50.18348/关注.js
@@ -0,0 +1,13 @@
+var EnginesList = []
+EnginesList.push(engines.execScript(Date.parse(new Date()).toString(), 'id("left_btn").findOne(10000).click()'));
+
+
+
+/*
+app.startActivity({
+    action: "android.intent.action.VIEW",
+    data: "kwai://profile/1340362263",
+    packgeName: "com.smile.gifmaker"
+});
+
+*/
